52a57d37451b43ad4abfdd895ba20c3aa17a3d1f
xiaoou <xiaoou@xiaoou.local>
2016-06-25 17:57:10 +0800

    first commit

---
+++ Pages/search/locationSearchViewController.h
@@ -0,0 +1,36 @@
+//
+//  locationSearchViewController.h
+//  Tutu
+//
+//  Created by gexing on 15/4/8.
+//  Copyright (c) 2015年 zxy. All rights reserved.
+//
+
+#import "NavBaseController.h"
+#import <CoreLocation/CoreLocation.h>
+//#import "BMKPoiSearch.h"
+#import <BaiduMapAPI_Base/BMKBaseComponent.h>
+#import <BaiduMapAPI_Map/BMKMapComponent.h>
+#import <BaiduMapAPI_Search/BMKSearchComponent.h>
+#import <BaiduMapAPI_Location/BMKLocationComponent.h>
+//#import <BaiduMapAPI/BMKPoiSearch.h>
+@protocol localDelegate <NSObject>
+
+-(void)searchPoiItemClick:(BMKPoiInfo *) info;
+
+
+@end
+
+@interface locationSearchViewController : NavBaseController<BMKLocationServiceDelegate>
+{
+    BMKPoiSearch *_poisearch;
+}
+
+@property(nonatomic,strong) BMKLocationService *locService;
+@property(nonatomic,assign)id<localDelegate>delegate;
+@property(nonatomic,strong)UITableView *mainTable;
+
+@property(nonatomic,strong) BMKPoiInfo *poiInfo;
+
+
+@end
